dcd1742991f5ac44ce0a8bbff476ca8ab74a5e9e
Alexey Anshakov <a.a@tutanota.com>
2023-07-15 18:55:33 +0200

    v1

--- addon/background.js
+++ addon/background.js
@@ -9,7 +9,13 @@ self.addEventListener('fetch', (event) => {
 chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
   if (request.criteria) {
     const criteria = request.criteria;
+    // Store the criteria in the extension's storage or perform any other required action
+    console.log('criteria received from the webpage:', criteria);
+
+    // Store the criteria in the extension's storage
     chrome.storage.local.set({ criteria: criteria }, function() {
+      console.log('Criteria stored in storage:', criteria);
+      // Send a response back to the webpage
       sendResponse({ success: true });
     });
   }
--- addon/followers.js
+++ addon/followers.js
@@ -1,13 +1,18 @@
 // Check if the current URL matches the followers page pattern
 function checkFollowersPage() {
   const currentURL = window.location.href;
+  console.log('currentURL:', currentURL);
 
   const followersPagePattern = /^https?:\/\/twitter\.com\/([^/]+)\/followers(\/\w+)?$/;
   const match = currentURL.match(followersPagePattern);
 
   if (match) {
     const username = match[1];
+    console.log('Followers page for:', username);
+
+    // Function to check elements and apply styles
     function checkElements(criteria) {
+      console.log('criteriaN:', criteria);
       const elements = document.querySelectorAll('[data-testid="UserCell"]');
       elements.forEach((element) => {
         const textElements = element.querySelectorAll('.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0');
@@ -16,6 +21,7 @@ function checkFollowersPage() {
           const text = textElement.textContent;
           mergedText += text + ' ';
         });
+        console.log('mergedText:', mergedText);
 
         const hasMatch = criteria.some((word) => mergedText.toLowerCase().includes(word.toLowerCase()));
 
@@ -30,34 +36,36 @@ function checkFollowersPage() {
     }
 
     // Function to continuously try finding elements
-    function tryFindingElements() {
-      const elements = document.querySelectorAll('[data-testid="cellInnerDiv"]');
-      if (elements.length > 0) {
-        clearInterval(intervalId);
-
-        chrome.storage.local.get('criteria', function(result) {
-          const initialCriteria = result.criteria;
-          // Use the criteria in your code as needed
-          const initialMatchWords = initialCriteria ? initialCriteria.split(',').map(word => word.trim()) : [];
-          //console.log('Initial Criteria:', initialMatchWords);
-          checkElements(initialMatchWords);
-        });
-      }
-    }
-
-    // Function to handle scroll event
-    function handleScroll() {
+  function tryFindingElements() {
+    const elements = document.querySelectorAll('[data-testid="cellInnerDiv"]');
+    if (elements.length > 0) {
       clearInterval(intervalId);
-      intervalId = setInterval(tryFindingElements, 1000);
-    }
 
-    // Perform the initial check on elements
-    let intervalId;
+      chrome.storage.local.get('criteria', function(result) {
+        const initialCriteria = result.criteria;
+        console.log('Retrieved criteria from storage:', initialCriteria);
+        // Use the criteria in your code as needed
+        //const initialCriteria = localStorage.getItem('criteria');
+        const initialMatchWords = initialCriteria ? initialCriteria.split(',').map(word => word.trim()) : [];
+        console.log('Initial Criteria:', initialMatchWords);
+        checkElements(initialMatchWords);
+      });
+    }
+  }
+
+  // Function to handle scroll event
+  function handleScroll() {
+    clearInterval(intervalId);
     intervalId = setInterval(tryFindingElements, 1000);
+  }
 
-    // Add scroll event listener
-    window.addEventListener('scroll', handleScroll);
-    }
+  // Perform the initial check on elements
+  let intervalId;
+  intervalId = setInterval(tryFindingElements, 1000);
+
+  // Add scroll event listener
+  window.addEventListener('scroll', handleScroll);
+  }
 }
 
 // Call the function to start checking on the followers page
