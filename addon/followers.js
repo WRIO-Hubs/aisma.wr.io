426576e9e3bda09a944ca4241396afd9e07509bd
Alexey Anshakov <a.a@tutanota.com>
2023-07-18 19:37:38 +0200

    Clear twitter contacts

--- addon/followers.js
+++ addon/followers.js
@@ -7,11 +7,11 @@ function checkFollowersPage() {
 
   if (match) {
     const username = match[1];
-    console.log('Followers page for:', username);
+    //console.log('Followers page for:', username);
 
     // Function to check elements and apply styles
     function checkElements(criteria) {
-      console.log('criteriaN:', criteria);
+      //console.log('criteriaN:', criteria);
       const elements = document.querySelectorAll('[data-testid="UserCell"]');
       elements.forEach((element) => {
         const textElements = element.querySelectorAll('.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0');
@@ -20,52 +20,87 @@ function checkFollowersPage() {
           const text = textElement.textContent;
           mergedText += text + ' ';
         });
-        console.log('mergedText:', mergedText);
+        //console.log('mergedText:', mergedText);
 
         const hasMatch = criteria.some((word) => mergedText.toLowerCase().includes(word.toLowerCase()));
 
-        if (hasMatch) {
+        if (hasMatch && !element.dataset.scanLinkAdded) {
           element.style.border = '1px solid green';
           element.style.backgroundColor = 'lightgreen';
-        } else {
-          element.style.border = 'none';
-          element.style.backgroundColor = 'initial';
+
+          const linkElement = element.querySelector('a.css-4rbku5.css-18t94o4.css-1dbjc4n.r-1loqt21.r-1wbh5a2.r-dnmrzs.r-1ny4l3l');
+          if (linkElement) {
+            const link = 'https://twitter.com' + linkElement.getAttribute('href');
+
+            const scanLink = document.createElement('a');
+            scanLink.setAttribute('href', link);
+            scanLink.innerText = 'Scan';
+            scanLink.style.margin = '4px';
+
+            const targetElement = element.querySelector('.css-18t94o4.css-1dbjc4n.r-1m3jxhj.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-15ysp7h.r-4wgw6l.r-1ny4l3l.r-ymttw5.r-o7ynqc.r-6416eg.r-lrvibr');
+            if (targetElement) {
+              const parentElement = targetElement.parentNode;
+              parentElement.insertBefore(scanLink, targetElement);
+              element.dataset.scanLinkAdded = true;
+            }
+
+            scanLink.addEventListener('click', (event) => {
+              event.preventDefault();
+              event.stopPropagation();
+              chrome.runtime.sendMessage({ openScanUrl: link });
+            });
+          }
         }
       });
     }
 
     // Function to continuously try finding elements
-  function tryFindingElements() {
-    const elements = document.querySelectorAll('[data-testid="cellInnerDiv"]');
-    if (elements.length > 0) {
-      clearInterval(intervalId);
+    function tryFindingElements() {
+      const elements = document.querySelectorAll('[data-testid="cellInnerDiv"]');
+      if (elements.length > 0) {
+        clearInterval(intervalId);
 
-      chrome.storage.local.get('criteria', function(result) {
-        const initialCriteria = result.criteria;
-        console.log('Retrieved criteria from storage:', initialCriteria);
-        // Use the criteria in your code as needed
-        //const initialCriteria = localStorage.getItem('criteria');
-        const initialMatchWords = initialCriteria ? initialCriteria.split(',').map(word => word.trim()) : [];
-        console.log('Initial Criteria:', initialMatchWords);
-        checkElements(initialMatchWords);
-      });
+        chrome.storage.local.get('criteria', function(result) {
+          const initialCriteria = result.criteria;
+          //console.log('Retrieved criteria from storage:', initialCriteria);
+          // Use the criteria in your code as needed
+          //const initialCriteria = localStorage.getItem('criteria');
+          const initialMatchWords = initialCriteria ? initialCriteria.split(',').map(word => word.trim()) : [];
+          //console.log('Initial Criteria:', initialMatchWords);
+          checkElements(initialMatchWords);
+        });
+      }
     }
-  }
 
-  // Function to handle scroll event
-  function handleScroll() {
-    clearInterval(intervalId);
+    // Function to handle scroll event
+    function handleScroll() {
+      clearInterval(intervalId);
+      intervalId = setInterval(tryFindingElements, 1000);
+    }
+
+    // Perform the initial check on elements
+    let intervalId;
     intervalId = setInterval(tryFindingElements, 1000);
-  }
 
-  // Perform the initial check on elements
-  let intervalId;
-  intervalId = setInterval(tryFindingElements, 1000);
-
-  // Add scroll event listener
-  window.addEventListener('scroll', handleScroll);
+    // Add scroll event listener
+    window.addEventListener('scroll', handleScroll);
   }
 }
 
 // Call the function to start checking on the followers page
 checkFollowersPage();
+
+// Listen for URL changes and call checkFollowersPage when needed
+function handleURLChange() {
+  let previousURL = window.location.href;
+
+  setInterval(() => {
+    const currentURL = window.location.href;
+    if (currentURL !== previousURL) {
+      checkFollowersPage();
+      previousURL = currentURL;
+    }
+  }, 1000);
+}
+
+handleURLChange();
